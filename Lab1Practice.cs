c94618bbe0bce6950db067ebf97edd7406e410d3
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-05-25 21:55:22 -0600

    Practice

---
+++ Lab1Practice.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Megyn";
+      Console.WriteLine(name);
+      int number = 24;
+      Console.WriteLine(number);
+     }
+  }
+}
