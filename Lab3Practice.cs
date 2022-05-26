d0cf11f9a5bd1611da5e16e3ab2f347ac323bc0e
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-05-25 22:07:07 -0600

    C# Data Types Practice

---
+++ Lab3Practice.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 2;               // integer (whole number)
+      double myDoubleNum = 3.14D;  // floating point number
+      char myLetter = 'Z';         // character
+      bool myBool = false;          // boolean
+      string myText = "Yo";     // string
+      Console.WriteLine(myNum);
+      Console.WriteLine(myDoubleNum);
+      Console.WriteLine(myLetter);
+      Console.WriteLine(myBool);
+      Console.WriteLine(myText);
+    }
+  }
+}
