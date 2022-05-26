0ec60f7fa5894b2e80056f01800961bf38399f46
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-05-25 22:13:39 -0600

    C# Operators Practice

---
+++ Lab4Practice.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 99999 + -99998;
+      Console.WriteLine(x);  
+     }
+  }
+}
