597d11ff4be2ba1553059277a36703589e8cda5d
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-05-26 18:18:27 -0600

    Lyrics of Still Alive from Portal

---
+++ C#SyntaxPractice.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This was a triumph");
+      Console.WriteLine("I'm making a note here: huge success.");
+      Console.WriteLine("It's hard to overstate my satisfaction");
+      Console.WriteLine("Aperture Science");
+      Console.WriteLine("We do what we must, because we can.");
+      Console.WriteLine("For the good of all of us");
+      Console.WriteLine("Except the ones who are dead");
+      Console.WriteLine("But there's no sense crying over every mistake");
+      Console.WriteLine("You just keep on trying till you run out of cake");
+      Console.WriteLine("And the science gets done and you make a neat gun");
+      Console.WriteLine("For the people who are Still Alive.");
+    }
+  }
+}
