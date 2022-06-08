b94cc10147880587cc4ae1932dc438842b102882
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-06-07 20:43:35 -0600

    While Loops Lab pt. 1

---
+++ WhileLoopsLab1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 5;
+      while (i < 10) 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+ }}
